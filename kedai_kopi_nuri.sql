b19579d4f6d76a4b056cd85a5a146b0dab748f1b
sultanmp <81198376+sultanmp@users.noreply.github.com>
2023-11-24 18:23:26 +0700

    Add files via upload

--- kedai_kopi_nuri.sql
+++ kedai_kopi_nuri.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 05, 2023 at 05:39 PM
+-- Generation Time: Nov 24, 2023 at 12:20 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `kedai kopi nuri`
+-- Database: `pendahuluan3`
 --
 
 -- --------------------------------------------------------
@@ -28,15 +28,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `customer` (
-  `ID_customer` char(6) NOT NULL,
-  `Nama_Customer` varchar(100) DEFAULT NULL
+  `id_customer` char(6) NOT NULL,
+  `nama_customer` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`ID_customer`, `Nama_Customer`) VALUES
+INSERT INTO `customer` (`id_customer`, `nama_customer`) VALUES
 ('CTR001', 'Budi Santoso'),
 ('CTR002', 'Sisil Triana'),
 ('CTR003', 'Davi Liam'),
@@ -51,13 +51,23 @@ INSERT INTO `customer` (`ID_customer`, `Nama_Customer`) VALUES
 
 CREATE TABLE `membership` (
   `id_membership` char(6) NOT NULL,
-  `no_telepon_customer` varchar(15) DEFAULT NULL,
+  `no_telp_customer` varchar(15) NOT NULL,
   `alamat_customer` varchar(150) DEFAULT NULL,
-  `tanggal_pembuatan_kartu_membership` date DEFAULT curdate(),
-  `tanggal_kedaluwarsa_kartu_membership` date DEFAULT NULL,
-  `total_poin` int(11) DEFAULT NULL,
-  `customer_id_customer` char(6) DEFAULT NULL
-) ;
+  `tanggal_pembuatan` date DEFAULT NULL,
+  `tanggal_kedaluwarsa` date DEFAULT NULL,
+  `total_poin` int(11) NOT NULL,
+  `m_id_customer` char(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `membership`
+--
+
+INSERT INTO `membership` (`id_membership`, `no_telp_customer`, `alamat_customer`, `tanggal_pembuatan`, `tanggal_kedaluwarsa`, `total_poin`, `m_id_customer`) VALUES
+('MBR001', '08123456789', 'Jl. Imam Bonjol', '2023-10-24', '2023-11-30', 0, 'CTR001'),
+('MBR002', '0812345678', 'Jl. Kelinci', '2023-10-24', '2023-11-30', 0, 'CTR002'),
+('MBR003', '081234567890', 'Jl. Abah Ojak', '2023-10-25', '2023-12-01', 2, 'CTR003'),
+('MBR004', '08987654321', 'Jl. Kenangan', '2023-10-26', '2023-12-02', 6, 'CTR005');
 
 -- --------------------------------------------------------
 
@@ -66,26 +76,26 @@ CREATE TABLE `membership` (
 --
 
 CREATE TABLE `menu_minuman` (
-  `ID_minuman` char(5) NOT NULL,
-  `Nama_minuman` varchar(50) DEFAULT NULL,
-  `Harga_minuman` float DEFAULT NULL
+  `id_minuman` char(6) NOT NULL,
+  `nama_minuman` varchar(50) NOT NULL,
+  `harga_minuman` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `menu_minuman`
 --
 
-INSERT INTO `menu_minuman` (`ID_minuman`, `Nama_minuman`, `Harga_minuman`) VALUES
-('MNM01', 'Expresso', 18000),
-('MNM02', 'Cappuccino', 20000),
-('MNM03', 'Latte', 21000),
-('MNM04', 'Americano', 19000),
-('MNM05', 'Mocha', 22000),
-('MNM06', 'Macchiato', 23000),
-('MNM07', 'Cold Brew', 21000),
-('MNM08', 'Iced Coffee', 18000),
-('MNM09', 'Affogato', 23000),
-('MNM10', 'Coffee Frappe', 22000);
+INSERT INTO `menu_minuman` (`id_minuman`, `nama_minuman`, `harga_minuman`) VALUES
+('MNM001', 'Expresso', 18000),
+('MNM002', 'Cappucino', 20000),
+('MNM003', 'Latte', 21000),
+('MNM004', 'Americano', 19000),
+('MNM005', 'Mocha', 22000),
+('MNM006', 'Macchiato', 23000),
+('MNM007', 'Cold Brew', 21000),
+('MNM008', 'Iced Coffe', 18000),
+('MNM009', 'Affogato', 23000),
+('MNM010', 'Coffe Frappe', 22000);
 
 -- --------------------------------------------------------
 
@@ -94,19 +104,20 @@ INSERT INTO `menu_minuman` (`ID_minuman`, `Nama_minuman`, `Harga_minuman`) VALUE
 --
 
 CREATE TABLE `pegawai` (
-  `NIK` char(16) NOT NULL,
-  `Nama_pegawai` varchar(100) DEFAULT NULL,
-  `Jenis_kelamin` char(1) DEFAULT NULL,
-  `Email` varchar(50) DEFAULT NULL,
-  `Umur` int(11) DEFAULT NULL,
-  `nomor_telepon` varchar(15) DEFAULT NULL
+  `nik` char(16) NOT NULL,
+  `nama_pegawai` varchar(100) NOT NULL,
+  `jenis_kelamin` char(1) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `umur` int(11) NOT NULL,
+  `telepon` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pegawai`
 --
 
-INSERT INTO `pegawai` (`NIK`, `Nama_pegawai`, `Jenis_kelamin`, `Email`, `Umur`, `nomor_telepon`) VALUES
+INSERT INTO `pegawai` (`nik`, `nama_pegawai`, `jenis_kelamin`, `email`, `umur`, `telepon`) VALUES
+('1111222233334444', 'Maimunah', 'P', 'maimunah@gmail.com', 25, '621234567'),
 ('1234567890123456', 'Naufal Raf', 'L', 'naufal@gmail.com', 19, '62123456789'),
 ('2345678901234561', 'Surinala', 'P', 'surinala@gmail.com', 24, '621234567890'),
 ('3456789012345612', 'Ben John', 'L', 'benjohn@gmail.com', 22, '6212345678');
@@ -118,25 +129,25 @@ INSERT INTO `pegawai` (`NIK`, `Nama_pegawai`, `Jenis_kelamin`, `Email`, `Umur`, 
 --
 
 CREATE TABLE `transaksi` (
-  `ID_transaksi` char(10) NOT NULL,
-  `Tanggal_transaksi` date DEFAULT NULL,
-  `Metode_pembayaran` varchar(15) DEFAULT NULL,
-  `Customer_ID_customer` char(6) DEFAULT NULL,
-  `Pegawai_NIK` char(16) DEFAULT NULL
+  `id_transaksi` char(10) NOT NULL,
+  `tanggal_transaksi` date NOT NULL,
+  `metode_pembayaran` varchar(15) NOT NULL,
+  `customer_id_customer` char(6) NOT NULL,
+  `pegawai_nik` char(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`ID_transaksi`, `Tanggal_transaksi`, `Metode_pembayaran`, `Customer_ID_customer`, `Pegawai_NIK`) VALUES
+INSERT INTO `transaksi` (`id_transaksi`, `tanggal_transaksi`, `metode_pembayaran`, `customer_id_customer`, `pegawai_nik`) VALUES
 ('TRX0000001', '2023-10-01', 'Kartu kredit', 'CTR002', '2345678901234561'),
 ('TRX0000002', '2023-10-03', 'Transfer bank', 'CTR004', '3456789012345612'),
 ('TRX0000003', '2023-10-05', 'Tunai', 'CTR001', '3456789012345612'),
 ('TRX0000004', '2023-10-15', 'Kartu debit', 'CTR003', '1234567890123456'),
 ('TRX0000005', '2023-10-15', 'E-wallet', 'CTR004', '1234567890123456'),
 ('TRX0000006', '2023-10-21', 'Tunai', 'CTR001', '2345678901234561'),
-('TRX0000007', '2023-10-03', 'Transfer bank', 'CTR004', '3456789012345612');
+('TRX0000007', '2023-10-03', 'Transfer bank', 'CTR004', '2345678901234561');
 
 -- --------------------------------------------------------
 
@@ -145,29 +156,29 @@ INSERT INTO `transaksi` (`ID_transaksi`, `Tanggal_transaksi`, `Metode_pembayaran
 --
 
 CREATE TABLE `transaksi_minuman` (
-  `TM_Menu_minuman_ID` char(6) NOT NULL,
-  `TM_Transaksi_ID` char(10) NOT NULL,
-  `Jumlah_cup` int(11) DEFAULT NULL
+  `tm_menu_minuman_id` char(6) NOT NULL,
+  `tm_transaksi_id` char(10) NOT NULL,
+  `jumlah_minuman` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `transaksi_minuman`
 --
 
-INSERT INTO `transaksi_minuman` (`TM_Menu_minuman_ID`, `TM_Transaksi_ID`, `Jumlah_cup`) VALUES
-('MNM01', 'TRX0000003', 3),
-('MNM01', 'TRX0000005', 1),
-('MNM03', 'TRX0000002', 2),
-('MNM03', 'TRX0000003', 1),
-('MNM03', 'TRX0000006', 2),
-('MNM04', 'TRX0000004', 2),
-('MNM05', 'TRX0000007', 1),
-('MNM06', 'TRX0000002', 1),
-('MNM06', 'TRX0000005', 2),
-('MNM07', 'TRX0000001', 1),
-('MNM09', 'TRX0000005', 1),
-('MNM10', 'TRX0000001', 1),
-('MNM10', 'TRX0000004', 1);
+INSERT INTO `transaksi_minuman` (`tm_menu_minuman_id`, `tm_transaksi_id`, `jumlah_minuman`) VALUES
+('MNM001', 'TRX0000003', 3),
+('MNM001', 'TRX0000005', 1),
+('MNM003', 'TRX0000002', 2),
+('MNM003', 'TRX0000003', 1),
+('MNM003', 'TRX0000006', 2),
+('MNM004', 'TRX0000004', 2),
+('MNM005', 'TRX0000002', 1),
+('MNM005', 'TRX0000007', 1),
+('MNM006', 'TRX0000005', 2),
+('MNM007', 'TRX0000001', 1),
+('MNM009', 'TRX0000005', 1),
+('MNM010', 'TRX0000001', 1),
+('MNM010', 'TRX0000004', 1);
 
 --
 -- Indexes for dumped tables
@@ -177,41 +188,41 @@ INSERT INTO `transaksi_minuman` (`TM_Menu_minuman_ID`, `TM_Transaksi_ID`, `Jumla
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`ID_customer`);
+  ADD PRIMARY KEY (`id_customer`);
 
 --
 -- Indexes for table `membership`
 --
 ALTER TABLE `membership`
   ADD PRIMARY KEY (`id_membership`),
-  ADD KEY `fk_customer_membership` (`customer_id_customer`);
+  ADD KEY `FK_customer_membership` (`m_id_customer`);
 
 --
 -- Indexes for table `menu_minuman`
 --
 ALTER TABLE `menu_minuman`
-  ADD PRIMARY KEY (`ID_minuman`);
+  ADD PRIMARY KEY (`id_minuman`);
 
 --
 -- Indexes for table `pegawai`
 --
 ALTER TABLE `pegawai`
-  ADD PRIMARY KEY (`NIK`);
+  ADD PRIMARY KEY (`nik`);
 
 --
 -- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`ID_transaksi`),
-  ADD KEY `Pegawai_NIK` (`Pegawai_NIK`),
-  ADD KEY `fk_customer_transaksi` (`Customer_ID_customer`);
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `FK_pegawai_transaksi` (`pegawai_nik`),
+  ADD KEY `FK_customer_transaksi` (`customer_id_customer`);
 
 --
 -- Indexes for table `transaksi_minuman`
 --
 ALTER TABLE `transaksi_minuman`
-  ADD PRIMARY KEY (`TM_Menu_minuman_ID`,`TM_Transaksi_ID`),
-  ADD KEY `TM_Transaksi_ID` (`TM_Transaksi_ID`);
+  ADD PRIMARY KEY (`tm_menu_minuman_id`,`tm_transaksi_id`),
+  ADD KEY `FK_transaksi_tm` (`tm_transaksi_id`);
 
 --
 -- Constraints for dumped tables
@@ -221,23 +232,21 @@ ALTER TABLE `transaksi_minuman`
 -- Constraints for table `membership`
 --
 ALTER TABLE `membership`
-  ADD CONSTRAINT `fk_customer_membership` FOREIGN KEY (`customer_id_customer`) REFERENCES `customer` (`ID_customer`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `membership_ibfk_1` FOREIGN KEY (`customer_id_customer`) REFERENCES `customer` (`ID_customer`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_customer_membership` FOREIGN KEY (`m_id_customer`) REFERENCES `customer` (`id_customer`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD CONSTRAINT `fk_customer_transaksi` FOREIGN KEY (`Customer_ID_customer`) REFERENCES `customer` (`ID_customer`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`Customer_ID_customer`) REFERENCES `customer` (`ID_customer`),
-  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`Pegawai_NIK`) REFERENCES `pegawai` (`NIK`);
+  ADD CONSTRAINT `FK_customer_transaksi` FOREIGN KEY (`customer_id_customer`) REFERENCES `customer` (`id_customer`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_pegawai_transaksi` FOREIGN KEY (`pegawai_nik`) REFERENCES `pegawai` (`nik`);
 
 --
 -- Constraints for table `transaksi_minuman`
 --
 ALTER TABLE `transaksi_minuman`
-  ADD CONSTRAINT `transaksi_minuman_ibfk_1` FOREIGN KEY (`TM_Menu_minuman_ID`) REFERENCES `menu_minuman` (`ID_minuman`),
-  ADD CONSTRAINT `transaksi_minuman_ibfk_2` FOREIGN KEY (`TM_Transaksi_ID`) REFERENCES `transaksi` (`ID_transaksi`);
+  ADD CONSTRAINT `FK_minuman_tm` FOREIGN KEY (`tm_menu_minuman_id`) REFERENCES `menu_minuman` (`id_minuman`),
+  ADD CONSTRAINT `FK_transaksi_tm` FOREIGN KEY (`tm_transaksi_id`) REFERENCES `transaksi` (`id_transaksi`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
